b81d3c2bd572db6433fa77d2c1cce6bff56927ca
Renz Angelo Priela <renzpriela59@gmail.com>
2025-10-24 00:22:36 +0800

    created initial main menu and added input validator

---
+++ ui/menu.py
@@ -0,0 +1,75 @@
+
+def main():
+    print("~~~~~  Main Menu  ~~~~~")
+    print("1. Manage Products")
+    print("2. Manage Categories")
+    print("0. Exit")
+
+    while True:
+        choice = int(input("Select an Option: "))
+        match choice:
+            case "1":
+                show_product_menu()
+            case "2":
+                show_category_menu()
+            case "0":
+                print("Exiting the program...")
+                break
+            case _:
+                print("Invalid choice. Please try again.")
+
+
+def show_product_menu():
+    print("~~~~~  Product Menu  ~~~~~")
+    print("1. Add Product")
+    print("2. View Products")
+    print("3. Update Product")
+    print("4. Delete Product")
+    print("0. Back to Main Menu")
+
+    while True:
+        choice = int(input("Select an Option: "))
+        match choice:
+            case "1":
+                print("Add Product...")
+            case "2":
+                print("View Products...")
+            case "3":
+                print("Update Product...")
+            case "4":
+                print("Delete Product...")
+            case "0":
+                print("Return to Main Menu...")
+                break
+            case _:
+                print("Invalid choice. Please try again.")
+
+
+def show_category_menu():
+    print("~~~~~  Category Menu  ~~~~~")
+    print("1. Add Category")
+    print("2. View Categories")
+    print("3. Update Category")
+    print("4. Delete Category")
+    print("0. Back to Main Menu")
+
+    while True:
+        choice = int(input("Select an Option: "))
+        match choice:
+            case "1":
+                print("Add Category...")
+            case "2":
+                print("View Categories...")
+            case "3":
+                print("Update Category...")
+            case "4":
+                print("Delete Category...")
+            case "0":
+                print("Return to Main Menu...")
+                break
+            case _:
+                print("Invalid choice. Please try again.")
+
+
+
+main()
